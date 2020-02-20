9e5cc36a6b79d68421c9c26095a5abc15e3cbba9
ruok27 <jakxii27@gmail.com>
2020-02-20 02:10:18 -0500

    demo

---
+++ build/precache-manifest.46b511789350141618cd94b80a71241d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c68b4aca6565fe486bff591057233ea",
+    "url": "/reactmemorygame/index.html"
+  },
+  {
+    "revision": "e720480daba5624d8c83",
+    "url": "/reactmemorygame/static/css/2.3db4eb7d.chunk.css"
+  },
+  {
+    "revision": "e720480daba5624d8c83",
+    "url": "/reactmemorygame/static/js/2.92134938.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactmemorygame/static/js/2.92134938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e2955bfe43eb7fb409",
+    "url": "/reactmemorygame/static/js/main.bdb95579.chunk.js"
+  },
+  {
+    "revision": "c0462d4878674f99f7ed",
+    "url": "/reactmemorygame/static/js/runtime-main.0af95296.js"
+  }
+]);
---
+++ src/components/Counter.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count -1 });
+  };
+
+  render() {
+    return (
+      <div className="card text-center">
+        <div className="card-header bg-primary text-white">
+          Click Counter!
+        </div>
+        <div className="card-body">
+          <p className="card-text">Click Count: {this.state.count}</p>
+          <button className="btn btn-primary" onClick={this.handleIncrement}>
+            Increment
+          </button>
+
+
+          <button className="btn btn-warning" onClick={this.handlede}>
+            other button
+          </button>
+
+          <button className="btn btn-danger" onClick={this.handlede}>
+            Decrement
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
