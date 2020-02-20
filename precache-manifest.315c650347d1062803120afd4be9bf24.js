16478232ebeb4318dd3ac60c31dd285525c7b4c2
ruok27 <jakxii27@gmail.com>
2020-02-20 02:05:52 -0500

    Updates

---
+++ precache-manifest.315c650347d1062803120afd4be9bf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cb355dd884f54d9cc2fd2318f1f3f1",
+    "url": "/ReactMemoryGame/index.html"
+  },
+  {
+    "revision": "e720480daba5624d8c83",
+    "url": "/ReactMemoryGame/static/css/2.3db4eb7d.chunk.css"
+  },
+  {
+    "revision": "e720480daba5624d8c83",
+    "url": "/ReactMemoryGame/static/js/2.92134938.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactMemoryGame/static/js/2.92134938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e2955bfe43eb7fb409",
+    "url": "/ReactMemoryGame/static/js/main.bdb95579.chunk.js"
+  },
+  {
+    "revision": "0a0b217810076bc64207",
+    "url": "/ReactMemoryGame/static/js/runtime-main.fa5d582a.js"
+  }
+]);
